7a5f8911565e981ea05f342c99b5c93b033e4d3f
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-08-25 11:57:21 +0300

    Create ProblemTag enumeration

---
+++ MathsXY.Infrastructure/Data/Types/ProblemTag.cs
@@ -0,0 +1,20 @@
+﻿namespace MathsXY.Infrastructure.Data.Types
+{
+    [Flags]
+    public enum ProblemTag
+    {
+        Arithmetic = 1,
+        Algebra = 2,
+        Geometry = 3,
+        Trigonometry = 4,
+        Calculus = 8,
+        ProbabilityAndStatistics = 16,
+        NumberTheory = 32,
+        DiscreteMaths = 64,
+        LinearAlgebra = 128,
+        MathsLogicAndFoundations = 256,
+        AppliedMaths = 512,
+        ComplexAnalysis = 1024,
+        DifferentialEquations = 2048
+    }
+}
