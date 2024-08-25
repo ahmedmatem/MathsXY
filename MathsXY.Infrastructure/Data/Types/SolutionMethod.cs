08f3cbdff3bd35538696ff83b8ea9bb6a23ccd40
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-08-25 13:03:20 +0300

    Add solution method types

---
+++ MathsXY.Infrastructure/Data/Types/SolutionMethod.cs
@@ -0,0 +1,12 @@
+﻿namespace MathsXY.Infrastructure.Data.Types
+{
+    /// <summary>
+    /// The method or formula used to solve the problem
+    /// </summary>
+    [Flags]
+    public enum SolutionMethod
+    {
+        QuadraticFormula = 1,
+        PythogoreanTheorem = 2
+    }
+}
