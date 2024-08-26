c5b9f8529cdc699cfb54c09be055025a7fd3ac4f
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-08-26 12:15:36 +0300

    Refactoring DbContext

--- MathsXY.Infrastructure/Data/MathsXYContext.cs
+++ MathsXY.Infrastructure/Data/MathsXYContext.cs
@@ -1,14 +1,14 @@
 ﻿namespace MathsXY.Infrastructure.Data
 {
-    using System;
     using System.Threading;
     using System.Threading.Tasks;
 
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-    using MathsXY.Infrastructure.Data.Models;
+    using MathsXY.Infrastructure.Extensions;
 
-    public class MathsXYContext : DbContext
+    public class MathsXYContext : IdentityDbContext
     {
         public MathsXYContext(DbContextOptions options) : base(options)
         {
@@ -16,30 +16,8 @@
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
-            ApplyAuditInfoRules();
+            this.ApplyAuditInfoRules();
             return base.SaveChangesAsync(cancellationToken);
-        }
-
-        private void ApplyAuditInfoRules()
-        {
-            foreach (var entry in ChangeTracker.Entries()
-                .Where(e => e.Entity is DataModel))
-            {
-                var entity = (DataModel)entry.Entity;
-                if (entry.State == EntityState.Added)
-                {
-                    entity.Id = Guid.NewGuid().ToString();
-                    entity.CreatedOn = DateTime.UtcNow;
-                }
-                else if (entry.State == EntityState.Modified)
-                {
-                    entity.ModifiedOn = DateTime.UtcNow;
-                }
-                else if (entry.State == EntityState.Deleted)
-                {
-                    entity.DeletedOn = DateTime.UtcNow;
-                }
-            }
         }
     }
 }
---
+++ MathsXY.Infrastructure/Extensions/IdentityDbContextExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace MathsXY.Infrastructure.Extensions
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using MathsXY.Infrastructure.Data.Models;
+
+    public static class IdentityDbContextExtensions
+    {
+        public static void ApplyAuditInfoRules(this IdentityDbContext context)
+        {
+            foreach (var entry in context.ChangeTracker.Entries()
+                .Where(e => e.Entity is DataModel))
+            {
+                var entity = (DataModel)entry.Entity;
+                if (entry.State == EntityState.Added)
+                {
+                    entity.Id = Guid.NewGuid().ToString();
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+                else if (entry.State == EntityState.Deleted)
+                {
+                    entity.DeletedOn = DateTime.UtcNow;
+                }
+            }
+        }
+    }
+}
